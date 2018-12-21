8c116614745f68a5f836e6d4093dde0f594f2394
Will M <wpmcclung@gmail.com>
2018-12-20 21:01:24 -0600

    updating lint, db sign in, first api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,8 +9,11 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/createteam", function(req, res) {
+    db.Example.create({
+      TeamName: req.body.text,
+      NumberofPlayers: req.body.text,
+    }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
