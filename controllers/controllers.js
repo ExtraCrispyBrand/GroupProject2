5fd30591b2a3e9731072897c9a4cc66104542ff0
Will M <wpmcclung@gmail.com>
2019-01-09 15:31:42 -0600

    hoping this appeases travis but looking doubtful because when adding it converts back to CRLF we may need to adjust our lint

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,22 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 router.index = (req, res) => {
-  res.render('index');
+  res.render("index");
 };
 
 router.about = (req, res) => {
-  res.render('about');
+  res.render("about");
 };
 
 router.contact = (req, res) => {
-  res.render('contact');
+  res.render("contact");
 };
 
 router.tourni = (req, res) => {
-  res.render('tourni');
+  res.render("tourni");
 };
-
-
 
 module.exports = router;
