ebbd7f81d8f7616cb720d0659d7775304764cf85
ivanaspasova <ivannaspasov@gmail.com>
2019-01-10 18:01:20 -0600

    fixed contact page

--- controllers/authenticationcontrollers.js
+++ controllers/authenticationcontrollers.js
@@ -1,28 +1,35 @@
 //route controllers for authentication based routes
-const express = require('express');
+const express = require("express");
 const authenticationRouter = express.Router();
 
 authenticationRouter.signup = (req, res) => {
-  res.render('signup');
+  res.render("signup");
 };
 
 authenticationRouter.signin = (req, res) => {
-  res.render('signin');
+  res.render("signin");
 };
 
 authenticationRouter.playerhome = (req, res) => {
-  res.render('playerhome', { user: req.user, isSponsor: req.user.accountType === 'sponsor', isPlayer: req.user.accountType === 'player' });
+  res.render("playerhome", {
+    user: req.user,
+    isSponsor: req.user.accountType === "sponsor",
+    isPlayer: req.user.accountType === "player"
+  });
   console.log(req.user);
 };
 
 authenticationRouter.sponsorhome = (req, res) => {
-  res.render('sponsorhome', { user: req.user, isSponsor: req.user.accountType === 'sponsor', isPlayer: req.user.accountType === 'player' });
+  res.render("sponsorhome", {
+    user: req.user,
+    isSponsor: req.user.accountType === "sponsor",
+    isPlayer: req.user.accountType === "player"
+  });
   console.log(req.user);
 };
 
-
 authenticationRouter.logout = (req, res) => {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 };
 module.exports = authenticationRouter;
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 router.index = (req, res) => {
-  res.render('index');
+  res.render("index");
 };
 
 router.about = (req, res) => {
-  res.render('about');
+  res.render("about");
 };
 
 router.contact = (req, res) => {
-  res.render('contact');
+  res.render("contact");
 };
 
 module.exports = router;
